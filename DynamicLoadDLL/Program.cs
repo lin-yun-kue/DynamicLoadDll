df1ba344860c663eec5bced8a781cb7800c15d3a
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2018-05-21 16:13:35 +0800

    dynamic load dll sample

---
+++ DynamicLoadDLL/Program.cs
@@ -0,0 +1,32 @@
+﻿using PluginInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLoadDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asmb =  Assembly.LoadFrom("Plugin1.dll");
+
+            var begin = DateTime.Now;
+            Console.WriteLine("Begin Time:" + begin);
+
+            //靜態繫結
+            var obj = (IPlugin)asmb.CreateInstance("Plugin1.Plugin1");
+            //動態繫結
+            //dynamic obj = asmb.CreateInstance("Plugin1.Plugin1");
+            obj.Execute();
+
+            var end = DateTime.Now;
+            Console.WriteLine("End Time:" + end);
+            Console.WriteLine("Total:" + (end - begin).ToString());
+            Console.Read();
+        }
+    }
+}
---
+++ Plugin1/Plugin1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PluginInterface;
+
+namespace Plugin1
+{
+    public class Plugin1 : IPlugin
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Plugin1");
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+    }
+}
